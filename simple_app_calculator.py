19d80f148a43beaa6f6c398c152cc7b1287a672c
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-28 22:58:37 +0800

    adding exception handling when user input a letter in the input or even the operation is invalid due to zero division. If this appears, the code will run again from the start

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,16 +1,23 @@
 while True:
-    num_1 = float(input("Enter a number: "))
-    num_2 = float(input("Enter another number: "))
+    try:
+        num_1 = float(input("Enter a number: "))
+        num_2 = float(input("Enter another number: "))
 
-    operation = input("Enter operation [+, -, *, /: ")
-    if operation == "+":
-        print(num_1 + num_2)
-    elif operation == "-":
-        print(num_1 - num_2)
-    elif operation == "*":
-        print(num_1 * num_2)
-    elif operation == "/":
-        print(num_1 / num_2)
+        operation = input("Enter operation [+, -, *, /: ")
+        if operation == "+":
+            print(num_1 + num_2)
+        elif operation == "-":
+            print(num_1 - num_2)
+        elif operation == "*":
+            print(num_1 * num_2)
+        elif operation == "/":
+            print(num_1 / num_2)
+    except ValueError:
+        print("Please enter a number")
+        continue
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
+        continue
 
     calculate_again = input("Do you want to calculate again? [y/n]: ")
     if calculate_again != "y":

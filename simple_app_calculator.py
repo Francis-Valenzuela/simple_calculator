03a43df4c3f5a9914e6140e845e64fdc855db1aa
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-28 22:52:19 +0800

    creating a while loop if user wants to calculate again

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,12 +1,17 @@
-num_1 = float(input("Enter a number: "))
-num_2 = float(input("Enter another number: "))
+while True:
+    num_1 = float(input("Enter a number: "))
+    num_2 = float(input("Enter another number: "))
 
-operation = input("Enter operation [+, -, *, /: ")
-if operation == "+":
-    print(num_1 + num_2)
-elif operation == "-":
-    print(num_1 - num_2)
-elif operation == "*":
-    print(num_1 * num_2)
-elif operation == "/":
-    print(num_1 / num_2)
+    operation = input("Enter operation [+, -, *, /: ")
+    if operation == "+":
+        print(num_1 + num_2)
+    elif operation == "-":
+        print(num_1 - num_2)
+    elif operation == "*":
+        print(num_1 * num_2)
+    elif operation == "/":
+        print(num_1 / num_2)
+
+    calculate_again = input("Do you want to calculate again? [y/n]: ")
+    if calculate_again != "y":
+        break

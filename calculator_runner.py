35f11bb299e737712d66871730bd16a261040fbe
unknown <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-04-29 21:58:39 +0800

    putting the remaining code where user needs to input the number, operation, and answer yes or no if to continue calculating or to store values

---
+++ calculator_runner.py
@@ -0,0 +1,39 @@
+from simple_app_calculator import SimpleCalculator
+class CalculatorRunner(SimpleCalculator):
+    def __init__(self):
+        self.stored_value = None
+
+    def run(self):
+        while True:
+            try:
+                using_stored_value = 'n'
+
+                if self.stored_value is not None:
+                    using_stored_value = input("Do you want to stored value? [y/n]: ")
+
+                if using_stored_value == 'y':
+                    num_1 = self.stored_value
+                else:
+                    num_1 = float(input("Please enter a number: "))
+                num_2 = float(input("Please enter another number: "))
+
+                operation = input('Enter operation [+, -, *, /]: ')
+
+                result = self.calculate(num_1, num_2, operation)
+
+                print('Result: ', result)
+
+                use_saved_value = input("Do you want to store value? [y/n]: ")
+                if use_saved_value == 'y':
+                    self.stored_value = result
+
+            except ValueError:
+                print("Please enter a numeric value or operation")
+                continue
+            except ZeroDivisionError:
+                print("Please enter a numeric value")
+                continue
+
+            calculate_again = input("Do you want to calculate again? [y/n]: ")
+            if calculate_again != 'y':
+                break
